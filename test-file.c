5385cf1473e8c19f41d17c1e4fb48ff17939103a
moth <moth@airmail.cc>
2017-08-05 18:19:20 -0400

    add more tests

--- test-file.c
+++ test-file.c
@@ -6,6 +6,7 @@
 static void create_handle(struct file *hand);
 static void read_handle(struct file *hand);
 static void read_buffered(struct file *hand);
+static void test_discard(struct file *hand);
 
 static struct file ghand[1];
 
@@ -18,6 +19,9 @@ struct unit_test tests[] = {
 	 .ctx = ghand,},
 	{.msg = "should do buffered reads",
 	 .fun = unit_list(create_handle, read_buffered),
+	 .ctx = ghand,},
+	{.msg = "should discard lines properly",
+	 .fun = unit_list(create_handle, test_discard),
 	 .ctx = ghand,},
 };
 
@@ -32,7 +36,26 @@ create_handle(struct file *hand)
 void
 read_buffered(struct file *hand)
 {
-	ok(0);
+	ok(dprintf(pipe_fd[1], "abc") > 0);
+
+	ok(file_readable(hand));
+
+	expect(0, hand->offset);
+	expect(0, hand->length);
+	ok(file_get_char(hand) == 'a');
+
+	ok(!file_readable(hand));
+
+	expect(1, hand->offset);
+	expect(3, hand->length);
+	ok(file_get_char(hand) == 'b');
+
+	expect(2, hand->offset);
+	expect(3, hand->length);
+	ok(file_get_char(hand) == 'c');
+
+	expect(0, hand->offset);
+	expect(0, hand->length);
 }
 
 void
@@ -44,6 +67,21 @@ read_handle(struct file *hand)
 	ok(!file_read_into_buffer(hand, buffer, 15));
 
 	ok(!strcmp(buffer, "doodly-do"));
+}
+
+void
+test_discard(struct file *hand)
+{
+	char buffer[256] = {0};
+	dprintf(pipe_fd[1], "me too thanks\n");
+
+	ok(!file_discard_line(hand));
+	ok(!file_readable(hand));
+
+	dprintf(pipe_fd[1], "ssssssssss\nhere we are");
+	ok(!file_discard_line(hand));
+	ok(!file_read_into_buffer(hand, buffer, 255));
+	ok(!strcmp(buffer, "here we are"));
 }
 
 int
