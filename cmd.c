4d9b1b8613acc8f6a3cef9d44bc6378cce8757f1
moth <moth@airmail.cc>
2017-08-07 13:02:03 -0400

    and another function

--- cmd.c
+++ cmd.c
@@ -11,6 +11,12 @@ struct command commands[] = {
 };
 
 void
+cmd_add(struct set *cmds, struct command *cmd)
+{
+	set_add_key(cmds, cmd->node, cmd->name, cmd->len);
+}
+
+void
 cmd_init(struct set *cmds)
 {
 	size_t i;
@@ -21,8 +27,14 @@ cmd_init(struct set *cmds)
 	}
 }
 
-void
-cmd_add(struct set *cmds, struct command *cmd)
+struct command *
+cmd_lookup(struct set *cmds, char *name, size_t len)
 {
-	set_add_key(cmds, cmd->node, cmd->name, cmd->len);
+	struct command *res;
+
+	res = set_query(cmds, name, len);
+
+	if (res->len != len) return 0;
+
+	return !memcmp(res->name, name, len) ? res : 0;
 }
--- cmd.h
+++ cmd.h
@@ -7,6 +7,7 @@ struct command;
 
 void cmd_add(struct set *cmds, struct command *cmd);
 void cmd_init(struct set *cmds);
+struct command *cmd_lookup(struct set *cmds, char *name, size_t len);
 
 struct command {
 	struct set_node *node;
