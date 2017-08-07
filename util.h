db081dc35c782624f505f00cdc252f0e956e6aaf
moth <moth@airmail.cc>
2017-08-07 07:53:54 -0400

    add some bit twiddling functions

--- util.h
+++ util.h
@@ -1,11 +1,30 @@
 #ifndef _edna_util_
 #define _edna_util_
 
+#define LONG_BIT (sizeof (unsigned long) * 8)
 #define arr_len(arr) (sizeof (arr) / sizeof *(arr))
 
+typedef unsigned long ulong;
+typedef unsigned int uint;
+
 static inline
-unsigned long
-umin(unsigned long a, unsigned long b)
+ulong
+uclz(ulong a)
+{
+	if (!a) return LONG_BIT;
+	return __builtin_clzl(a);
+}
+
+static inline
+ulong
+ufls(ulong a)
+{
+	return LONG_BIT - uclz(a);
+}
+
+static inline
+ulong
+umin(ulong a, ulong b)
 {
 	return a < b ? a : b;
 }
