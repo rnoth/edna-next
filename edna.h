fa8755a2fabe5b86435246cbfe370198ad000636
moth <moth@airmail.cc>
2017-08-08 11:25:57 -0400

    forgot prototype

--- edna.h
+++ edna.h
@@ -3,8 +3,9 @@
 
 struct edna;
 
-int edna_init(struct edna *edna);
 void edna_fini(struct edna *edna);
+int edna_init(struct edna *edna);
+int edna_text_insert(struct edna *edna, size_t offset, char *text, size_t length);
 
 struct edna {
 	struct piece *text;
