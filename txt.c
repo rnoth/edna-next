9cc5ba9184dc1862a2e51f38121ce3b6536b3068
moth <moth@airmail.cc>
2017-08-09 11:46:58 -0400

    remove #if 0 block

--- txt.c
+++ txt.c
@@ -74,39 +74,6 @@ text_delete(struct piece **dest, size_t offset, size_t extent)
 	}
 
 	return 0;
-#if 0
-	struct piece *end[2];
-	struct piece *next;
-	size_t ext;
-	int err;
-
-	if (dest[0]->length >= offset + extent) {
-		err = text_split(dest, offset, extent);
-		if (err) return err;
-		dest[0] = 0, dest[1] = 0;
-		return 0;
-	}
-
-	end[0] = dest[0], end[1] = dest[1];
-	ext = text_walk(end, extent + offset);
-
-	end[0]->length -= ext;
-	end[0]->buffer += ext;
-
-	dest[0]->length = offset;
-
-	if (end[1] != dest[0]) {
-		text_unlink(end[0], end[1]);
-		next = text_next(dest[0], dest[1]);
-		text_link(next, dest[0]);
-
-		text_link(dest[0], end[0]);
-
-		dest[0] = next, dest[1] = 0;
-	}
-
-	return 0;
-#endif
 }
 
 void
