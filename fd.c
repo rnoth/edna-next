57db7a90a99a053009ea5b473460a18c59e71d19
moth <moth@airmail.cc>
2017-08-12 13:39:40 -0400

    add a new function: fd_read()

--- fd.c
+++ fd.c
@@ -1,6 +1,8 @@
 #include <errno.h>
+#include <stdlib.h>
 #include <poll.h>
 #include <sys/ioctl.h>
+#include <unistd.h>
 
 #include <fd.h>
 #include <util.h>
@@ -15,6 +17,29 @@ fd_peek(int fd)
 	if (err == -1) return 0;
 
 	return nbytes;
+}
+
+int
+fd_read(struct read *dest, int fd)
+{
+	ssize_t res;
+	int err;
+
+	err = fd_wait(fd);
+	if (err) return err;
+
+	dest->length = fd_peek(fd);
+
+	dest->buffer = malloc(dest->length);
+	if (!dest->buffer) return errno;
+
+	dest->offset = 0;
+
+	res = read(fd, dest->buffer, dest->length);
+	if (res == -1) return errno;
+	if (res == 0) return -1;
+
+	return 0;
 }
 
 bool
--- fd.h
+++ fd.h
@@ -5,7 +5,15 @@
 #include <stdio.h>
 #include <util.h>
 
+struct read {
+	size_t length;
+	size_t offset;
+	char *buffer;
+};
+
+void file_init(int fd);
 uint fd_peek(int fd);
+int fd_read(struct read *dest, int fd);
 bool fd_readable(int fd);
 int fd_wait(int fd);
 
