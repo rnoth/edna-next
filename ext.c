64888cd321abfdb68457aa432e6692f390ffb028
moth <moth@airmail.cc>
2017-08-15 09:04:35 -0400

    add a extents library

---
+++ ext.c
@@ -0,0 +1,193 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include <util.h>
+
+struct ext {
+	uintptr_t root;
+};
+
+struct ext_node {
+	uintptr_t chld[2];
+	size_t sum;
+	size_t ext;
+};
+
+struct walker {
+	uintptr_t prev;
+	uintptr_t tag;
+	size_t sum;
+};
+
+static bool is_node(uintptr_t tag);
+static struct ext_node *node_from_tag(uintptr_t tag);
+static uintptr_t tag_node(struct ext_node *node);
+static uintptr_t tag_back(uintptr_t tag);
+
+static ulong sig(ulong a) {return a & -a;} 
+
+static bool is_back(uintptr_t tag) { return !!(tag & 2); }
+static bool is_leaf(uintptr_t tag) { return tag & 1; }
+static bool is_node(uintptr_t tag) { return !(tag & 1); }
+static bool is_ext(uintptr_t tag) { return tag & 1; }
+
+static struct ext *ext_from_tag(uintptr_t tag) { return (void *)(tag & ~3); }
+static struct ext_node *node_from_tag(uintptr_t tag) {return (void *)(tag & ~3); }
+static uintptr_t tag_from_back(uintptr_t back) { return back & ~2; }
+
+static uintptr_t tag_back(uintptr_t tag) { return tag | 2; }
+static uintptr_t tag_leaf(struct ext_node *leaf) { return (uintptr_t)leaf | 1; }
+static uintptr_t tag_node(struct ext_node *node) { return (uintptr_t)node; }
+static uintptr_t tag_ext(struct ext *ext) { return (uintptr_t)ext | 1; }
+
+static void node_insert(struct walker *walker, struct ext_node *new_node);
+
+static void walker_begin(struct walker *walker, struct ext *ext);
+static void walker_rise(struct walker *walker);
+static void walker_surface(struct walker *walker);
+static void walker_walk(struct walker *walker, size_t p);
+
+void
+node_insert(struct walker *walker, struct ext_node *new_node)
+{
+	struct ext_node *node;
+	struct ext *ext;
+	size_t new_end;
+	size_t new_crit;
+	size_t end;
+	size_t crit;
+
+	node = node_from_tag(walker->tag);
+	end = walker->sum + node->ext;
+	new_node->sum = end;
+
+	new_end = end + new_node->ext;
+	new_crit = sig(new_end ^ end);
+
+	while (is_node(walker->prev)) {
+		node = node_from_tag(walker->prev);
+		end = walker->sum + node->ext;
+		crit = sig(new_end ^ end);
+
+		if (new_crit < crit) {
+			new_node->chld[0] = walker->tag;
+			new_node->chld[1] = tag_leaf(new_node);
+			walker->tag = tag_node(new_node);
+			return;
+		}
+
+		walker_rise(walker);
+	}
+
+	ext = ext_from_tag(walker->prev);
+	new_node->chld[0] = ext->root;
+	new_node->chld[1] = tag_leaf(new_node);
+	ext->root = tag_node(new_node);
+}
+
+void
+walker_begin(struct walker *walker, struct ext *ext)
+{
+	walker->prev = tag_ext(ext);
+	walker->tag = ext->root;
+	walker->sum = 0;
+}
+
+void
+walker_rise(struct walker *walker)
+{
+	struct ext_node *node;
+	int b;
+
+	if (is_ext(walker->prev)) return;
+
+	node = node_from_tag(walker->prev);
+	b = is_back(node->chld[1]);
+
+	walker->prev = tag_from_back(node->chld[b]);
+	node->chld[b] = walker->tag;
+
+	walker->tag = tag_node(node);
+	walker->sum -= node->chld[b];
+}
+
+void
+walker_surface(struct walker *walker)
+{
+	while (!is_ext(walker->prev)) {
+		walker_rise(walker);
+	}
+}
+
+void
+walker_visit(struct walker *walker, int b)
+{
+	struct ext_node *node;
+	uintptr_t next;
+
+	if (is_leaf(walker->tag)) return;
+
+	node = node_from_tag(walker->tag);
+	walker->sum += b ? node->sum : 0;
+
+	next = node->chld[b];
+	node->chld[b] = tag_back(walker->prev);
+
+	walker->prev = walker->tag;
+	walker->tag = next;
+}
+
+void
+walker_walk(struct walker *walker, size_t p)
+{
+	struct ext_node *node;
+	int b;
+
+	while (is_node(walker->tag)) {
+		node = node_from_tag(walker->tag);
+		b = p >= walker->sum + node->sum;
+		walker_visit(walker, b);
+	}
+}
+
+void
+ext_append(struct ext *ext, struct ext_node *new_node)
+{
+	struct walker walker[1];
+
+	new_node->sum = 0;
+
+	if (!ext->root) {
+		ext->root = tag_leaf(new_node);
+		return;
+	}
+
+	walker_begin(walker, ext);
+	walker_walk(walker, ~0);
+
+	node_insert(walker, new_node);
+
+	walker_surface(walker);
+	return;
+}
+
+struct ext_node *
+ext_stab(struct ext *ext, size_t point)
+{
+	struct walker walker[1];
+	struct ext_node *result;
+	struct ext_node *leaf;
+
+	if (!ext->root) return 0x0;
+
+	walker_begin(walker, ext);
+	walker_walk(walker, point);
+
+	leaf = node_from_tag(walker->tag);
+	result = walker->sum + leaf->ext > point ? leaf : 0x0;
+
+	walker_surface(walker); // TODO: remove walker stuff from here
+
+	return result;
+}
---
+++ test-ext.c
@@ -0,0 +1,73 @@
+#include <unit.h>
+#include <ext.c>
+
+#include <util.h>
+
+void
+test_double_add(void)
+{
+	struct ext_node a[1]={{.sum=3}};
+	struct ext_node b[1]={{.sum=1}};
+	struct ext_node c[1]={{.sum=7}};
+	struct ext ext[1]={0};
+
+	ext_append(ext, a);
+	ext_append(ext, b);
+	try(ext_append(ext, c));
+
+	ok(ext->root == tag_node(c));
+	ok(c->chld[0] == tag_node(b));
+	ok(c->chld[1] == tag_leaf(c));
+
+	ok(b->chld[0] == tag_leaf(a));
+	ok(b->chld[1] == tag_leaf(b));
+}
+
+void
+test_simple_add(void)
+{
+	struct ext_node a[1]={{.ext=1}};
+	struct ext_node b[1]={{.ext=2}};
+	struct ext ext[1]={0};
+
+	try(ext_append(ext, a));
+	ok(ext->root == tag_leaf(a));
+
+	try(ext_append(ext, b));
+	ok(ext->root == tag_node(b));
+	ok(b->chld[0] == tag_leaf(a));
+	ok(b->chld[1] == tag_leaf(b));
+}
+
+void
+test_query(void)
+{
+	struct ext_node a[1]={{.ext=22}};
+	struct ext_node b[1]={{.ext=455}};
+	struct ext_node c[1]={{.ext=123}};
+	struct ext ext[1]={0};
+
+	try(ext_append(ext, a));
+	try(ext_append(ext, b));
+	try(ext_append(ext, c));
+
+	ok(ext_stab(ext, 11) == a);
+	ok(ext_stab(ext, 470) == b);
+	ok(ext_stab(ext, 599) == c);
+}
+
+static struct unit_test tests[] = {
+	{.msg = "should be able append a single extent",
+	 .fun = unit_list(test_simple_add),},
+	{.msg = "should be able to append two extents",
+	 .fun = unit_list(test_double_add),},
+	{.msg = "should be able to query extents",
+	 .fun = unit_list(test_query),},
+};
+
+int
+main(int argc, char **argv)
+{
+	unit_parse_argv(argv);
+	return unit_run_tests(tests, arr_len(tests));
+}
