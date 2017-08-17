f7a5f0d9ec4ef90e7e8e0ffb3f432a651ffb2b34
moth <moth@airmail.cc>
2017-08-17 16:25:57 -0400

    add a new member to struct edna

--- edna.h
+++ edna.h
@@ -1,6 +1,7 @@
 #ifndef _edna_
 #define _edna_
 
+#include <ext.h>
 #include <set.h>
 
 struct edna;
@@ -12,6 +13,7 @@ int edna_text_insert(struct edna *edna, size_t offset, char *text, size_t length
 struct edna {
 	struct piece *text;
 	struct piece *dead;
+	struct ext lines[1];
 	struct set cmds[1];
 };
 
