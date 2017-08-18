94249154cc863f9ec0de1714923fbcfbec72fb6a
moth <moth@airmail.cc>
2017-08-18 10:12:43 -0400

    sketch out the undo record facility

--- edna.c
+++ edna.c
@@ -5,6 +5,18 @@
 #include <edna.h>
 #include <cmd.h>
 #include <txt.h>
+#include <util.h>
+
+struct record {
+	struct record *prev;
+	size_t length;
+	struct {
+		size_t length;
+		char *buffer;
+	} chain[];
+};
+
+static int rec_marshal(struct record **dest, struct piece *txt);
 
 void
 edna_fini(struct edna *edna)
@@ -48,9 +60,14 @@ edna_text_delete(struct edna *edna, size_t offset, size_t extent)
 int
 edna_text_insert(struct edna *edna, size_t offset, char *text, size_t length)
 {
+	struct record *_;
 	struct piece *links[2];
 	struct piece *pie;
 	int err;
+
+	err = rec_marshal(&_, edna->text);
+	if (err > 0) return err;
+	if (!err) free(_);
 
 	pie = calloc(1, sizeof *pie);
 	if (!pie) return ENOMEM;
@@ -69,3 +86,28 @@ edna_text_insert(struct edna *edna, size_t offset, char *text, size_t length)
 	return 0;
 }
 
+int
+rec_marshal(struct record **dest, struct piece *txt)
+{
+	struct piece *t[2];
+	size_t i;
+
+	t[0] = text_next(txt, 0), t[1] = txt;
+
+	for (i=0; text_next(t[0], t[1]); ++i) text_step(t);
+	if (!i) return -1;
+
+	*dest = calloc(1, sizeof **dest + i * sizeof *dest[0]->chain);
+	if (!*dest) return ENOMEM;
+
+	dest[0]->length = i;
+
+	ptr_swap(t+0, t+1);
+	while (--i, text_next(t[0], t[1])) {
+		text_step(t);
+		dest[0]->chain[i].length = t[1]->length;
+		dest[0]->chain[i].buffer = t[1]->buffer;
+	}
+
+	return 0;
+}
