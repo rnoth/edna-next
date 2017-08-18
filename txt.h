21267d14431e5f4090f9511fdc6cfd494488b0f4
moth <moth@airmail.cc>
2017-08-18 17:36:07 -0400

    forgot text_merge() prototype

--- txt.h
+++ txt.h
@@ -25,6 +25,7 @@ int text_delete(struct piece **dest, size_t offset, size_t extent);
 int text_insert(struct piece **dest, size_t offset, char *buffer, size_t length);
 
 void text_link(struct piece *lef, struct piece *rit);
+void text_merge(struct piece **ctx);
 struct piece *text_next(struct piece *cur, struct piece *prev);
 void text_step(struct piece **links);
 void text_unlink(struct piece *lef, struct piece *rit);
