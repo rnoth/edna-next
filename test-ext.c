e6e77146c9d405a93310f49c15ce83b44fa46a8b
moth <moth@airmail.cc>
2017-08-19 11:14:18 -0400

    test the new iteration functions

--- test-ext.c
+++ test-ext.c
@@ -6,6 +6,8 @@
 static void test_add_two(void);
 static void test_add_simple(void);
 static void test_insert(void);
+static void test_iter(void);
+static void test_iter_edge(void);
 static void test_query(void);
 
 static struct unit_test tests[] = {
@@ -17,6 +19,10 @@ static struct unit_test tests[] = {
 	 .fun = unit_list(test_query),},
 	{.msg = "should be able to insert extents",
 	 .fun = unit_list(test_insert),},
+	{.msg = "should be able to iterate over an extent tree",
+	 .fun = unit_list(test_iter),},
+	{.msg = "should handle edge cases with iterating",
+	 .fun = unit_list(test_iter_edge),}
 };
 
 void
@@ -90,6 +96,43 @@ test_insert(void)
 
 	ok(c->chld[0] == tag_leaf(c));
 	ok(c->chld[1] == tag_leaf(a));
+}
+
+void
+test_iter(void)
+{
+	struct ext_node a[1]={{.ext=100}};
+	struct ext_node b[1]={{.ext=250}};
+	struct ext_node c[1]={{.ext=75}};
+	struct ext_walker w[1];
+	struct ext x[1]={{0}};
+
+	ext_insert(x, a, 0);
+	ext_insert(x, b, 0);
+	ext_insert(x, c, 250);
+
+	ok(ext_iterate(w, x) == b);
+	ok(ext_continue(w) == c);
+	ok(ext_continue(w) == a);
+	ok(ext_continue(w) == 0);
+	ok(ext_continue(w) == 0);
+}
+
+void
+test_iter_edge(void)
+{
+	struct ext empty[1]={{0}};
+	struct ext smol[1]={{0}};
+	struct ext_node leaf[1]={{.ext=56}};
+	struct ext_walker w[1];
+	
+	ok(ext_iterate(w, empty) == 0);
+	ok(ext_continue(w) == 0);
+
+	ext_append(smol, leaf);
+
+	ok(ext_iterate(w, smol) == leaf);
+	ok(ext_continue(w) == 0);
 }
 
 void
