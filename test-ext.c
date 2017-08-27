e75397422a12982dd07867c083062fdd489e2fb1
moth <moth@airmail.cc>
2017-08-27 09:51:38 -0400

    better tests

--- test-ext.c
+++ test-ext.c
@@ -3,19 +3,31 @@
 
 #include <util.h>
 
-static void test_add_two(void);
-static void test_add_simple(void);
+static void test_append_three(void);
+static void test_append_single(void);
+static void test_append_left(void);
+static void test_append_right(void);
 static void test_insert(void);
+static void test_free_none(void);
+static void test_free_one(void);
+static void test_free_many(void);
 static void test_next(void);
 static void test_remove(void);
 static void test_remove_relink(void);
 static void test_query(void);
+static void test_walker_begin(void);
 
 static struct unit_test tests[] = {
-	{.msg = "should be able append a single extent",
-	 .fun = unit_list(test_add_simple),},
+	{.msg = "should free a ext tree",
+	 .fun = unit_list(test_free_none, test_free_one, test_free_many),},
+	{.msg = "should append a single extent",
+	 .fun = unit_list(test_append_single),},
 	{.msg = "should append two extents",
-	 .fun = unit_list(test_add_two),},
+	 .fun = unit_list(test_append_left, test_append_right),},
+	{.msg = "should append three extents",
+	 .fun = unit_list(test_append_three),},
+	{.msg = "should initialise a walker",
+	 .fun = unit_list(test_walker_begin),},
 	{.msg = "should query extents",
 	 .fun = unit_list(test_query),},
 	{.msg = "should insert extents",
@@ -29,20 +41,78 @@ static struct unit_test tests[] = {
 };
 
 void
-test_add_two(void)
+test_append_left(void)
+{
+	struct ext_node a[1]={{.ext=10}};
+	struct ext_node b[1]={{.ext=5}};
+	struct ext ext[1]={0};
+
+	try(ext_append(ext, a));
+	try(ext_append(ext, b));
+
+	expect(0, ext->off);
+	expect(15, ext->len);
+	ok(ext->root == tag_node(b));
+
+	ok(b->off == 10);
+	ok(b->ext == 5);
+	ok(b->chld[0] == tag_leaf(a));
+	ok(b->chld[1] == tag_leaf(b));
+
+	ok(!a->chld[0] && !a->chld[1]);
+	ok(a->ext == 10);
+}
+
+void
+test_append_right(void)
+{
+	struct ext_node a[1]={{.ext=5}};
+	struct ext_node b[1]={{.ext=10}};
+	struct ext ext[1]={0};
+
+	try(ext_append(ext, a));
+	try(ext_append(ext, b));
+
+	ok(ext->root == tag_node(b));
+	expect(15, ext->len);
+	expect(0, ext->off);
+
+	ok(b->off == 5);
+	ok(b->ext == 10);
+	ok(b->chld[0] == tag_leaf(a));
+	ok(b->chld[1] == tag_leaf(b));
+
+	ok(!a->chld[0] && !a->chld[1]);
+	ok(a->ext == 5);
+}
+
+void
+test_append_single(void)
+{
+	struct ext_node a[1]={{.ext=1}};
+	struct ext ext[1]={0};
+
+	try(ext_append(ext, a));
+	ok(ext->root == tag_leaf(a));
+	expect(1, ext->len);
+	expect(1, a->off);
+	expect(0, ext->off);
+	ok(!a->chld[0] && !a->chld[1]);
+}
+
+void
+test_append_three(void)
 {
 	struct ext_node a[1]={{.ext=3}};
 	struct ext_node b[1]={{.ext=1}};
 	struct ext_node c[1]={{.ext=7}};
-	struct ext ext[1]={0};
+	struct ext x[1]={0};
 
-	ext_append(ext, a);
-	ext_append(ext, b);
-	expect(4, ext->len);
-
-	try(ext_append(ext, c));
-	expect(11, ext->len);
-	ok(ext->root = tag_node(b));
+	try(ext_append(x, a));
+	try(ext_append(x, b));
+	try(ext_append(x, c));
+	expect(11, x->len);
+	ok(x->root = tag_node(b));
 
 	ok(b->chld[0] == tag_leaf(a));
 	ok(b->chld[1] == tag_node(c));
@@ -52,24 +122,39 @@ test_add_two(void)
 }
 
 void
-test_add_simple(void)
+test_free_none(void)
 {
-	struct ext_node a[1]={{.ext=1}};
-	struct ext_node b[1]={{.ext=2}};
-	struct ext ext[1]={0};
+	struct ext x[1] = {{0}};
 
-	try(ext_append(ext, a));
-	ok(ext->root == tag_leaf(a));
-	expect(1, ext->len);
+	ext_free(x);
+}
 
-	try(ext_append(ext, b));
-	ok(ext->root == tag_node(b));
-	expect(3, ext->len);
+void
+test_free_one(void)
+{
+	struct ext_node *d;
+	struct ext x[1];
 
-	expect(1, b->off);
-	ok(b->chld[0] == tag_leaf(a));
-	ok(b->chld[1] == tag_leaf(b));
+	ok(d = calloc(1, sizeof *d));
+	x->root = tag_leaf(d);
 
+	ext_free(x);
+}
+
+void
+test_free_many(void)
+{
+	struct ext_node *d;
+	struct ext x[1]={{0}};
+	size_t i;
+
+	for (i=0; i<16; ++i) {
+		ok(d = calloc(1, sizeof *d));
+		d->ext = rand();
+		ext_append(x, d);
+	}
+
+	ext_free(x);
 }
 
 void
@@ -204,6 +289,24 @@ test_query(void)
 	ok(ext_stab(ext, 11) == a);
 	ok(ext_stab(ext, 470) == b);
 	ok(ext_stab(ext, 599) == c);
+}
+
+void
+test_walker_begin(void)
+{
+	struct ext x[1]={{
+			.len=123,
+			.off=4,
+			.root=0xcafebabe,
+		}};
+	struct ext_walker w[1];
+
+	walker_begin(w, x);
+	ok(w->off == 4);
+	ok(w->len == 123);
+
+	ok(w->prev == tag_root(x));
+	ok(w->tag == 0xcafebabe);
 }
 
 int
