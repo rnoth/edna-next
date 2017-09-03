1342a6cb3cf389d7853354eac0a1bd23b924dbaf
moth <moth@airmail.cc>
2017-09-03 09:53:27 -0400

    Test ln.c.

---
+++ test-ln.c
@@ -0,0 +1,59 @@
+#include <unit.h>
+#include <util.h>
+#include <ln.c>
+
+static void test_adjust(void);
+static void test_convert(void);
+
+struct unit_test tests[] = {
+	{.msg = "should convert lines to buffers",
+	 .fun = unit_list(test_convert),},
+	{.msg = "should adjust lines",
+	 .fun = unit_list(test_adjust),},
+};
+
+void
+test_adjust(void)
+{
+	struct ext lines[1]={{0}};
+	struct ext_node *d;
+	struct ext_node *e;
+	char s[]="---><---";
+	char t[]="~";
+
+	ln_insert(lines, 0, s, sizeof s);
+	ln_insert(lines, 3, t, sizeof t);
+
+	try(d = ext_stab(lines, 1));
+	try(e = ext_stab(lines, 3));
+	ok(e == d);
+	expect(9, d->ext);
+}
+
+void
+test_convert(void)
+{
+	struct ext_node *list;
+	struct ext_node *node;
+	char s[]="one\ntwo\nthree\n";
+	size_t n = sizeof s - 1;
+	size_t f=n;
+	char *t=s+f;
+
+	ok(list = nodes_from_lines(s, n));
+	foreach_node(node, list) {
+		f -= node->ext;
+		t -= node->ext;
+		okf(!strcmp(t, s + f),
+		    "extent mismatch. s=%s, t=%s", s + f, t);
+	}
+
+	foreach_node(node, list) free(node);
+}
+
+int
+main(int argc, char **argv)
+{
+	unit_parse_argv(argv);
+	return unit_run_tests(tests, arr_len(tests));
+}
